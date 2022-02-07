e5a3d7646e3f9fed686736491b60f3845c023a28
Lucy Suddenly <suddenlylucy@gmail.com>
2022-02-07 13:18:16 -0800

    env structure

--- app/services/twitter_client.rb
+++ app/services/twitter_client.rb
@@ -5,19 +5,13 @@ class TwitterClient
 
   def client
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key    = credentials[:API_KEY]
-      config.consumer_secret = credentials[:API_KEY_SECRET]
-      config.bearer_token    = credentials[:BEARER_TOKEN]
+      config.consumer_key    = Rails.application.credentials[:TWITTER][:API_KEY]
+      config.consumer_secret = Rails.application.credentials[:TWITTER][:API_KEY_SECRET]
+      config.bearer_token    = Rails.application.credentials[:TWITTER][:BEARER_TOKEN]
     end
   end
 
   def method_missing(m, *args, &block)
     client.send(m, *args, &block)
-  end
-
-  private
-
-  def credentials
-    @credentials ||= Rails.application.credentials.TWITTER
   end
 end
